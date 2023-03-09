5282c37abe616acc7d95d11389372ee7910a4e87
Sentry00 <127325949+Sentry00@users.noreply.github.com>
2023-03-08 21:31:19 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+document.getElementById("listaContacto-click1").onmouseover = function () {
+    document.getElementById("listaContacto-click1").style.color = "#3AE4C7";
+    document.getElementById("listaContacto-click1").innerHTML = "Hipo.lopez@gmail.com";
+}
+document.getElementById("listaContacto-click1").onmouseout = function () {
+    document.getElementById("listaContacto-click1").style.color = "#0a144d";
+    document.getElementById("listaContacto-click1").innerHTML = "Email: Hipo.lopez@gmail.com";
+}
+document.getElementById("listaContacto-click2").onmouseover = function () {
+    document.getElementById("listaContacto-click2").style.color = "#3AE4C7";
+    document.getElementById("listaContacto-click2").innerHTML = "(585) 443-2141";
+}
+document.getElementById("listaContacto-click2").onmouseout = function () {
+    document.getElementById("listaContacto-click2").style.color = "#0a144d";
+    document.getElementById("listaContacto-click2").innerHTML = "Teléfono: (585) 443-2141";
+}
+document.getElementById("listaContacto-click3").onmouseover = function () {
+    document.getElementById("listaContacto-click3").style.color = "#3AE4C7";
+    document.getElementById("listaContacto-click3").innerHTML = "El carmen - Jujuy";
+}
+document.getElementById("listaContacto-click3").onmouseout = function () {
+    document.getElementById("listaContacto-click3").style.color = "#0a144d";
+    document.getElementById("listaContacto-click3").innerHTML = "Localidad: El carmen - Jujuy";
+}
+document.getElementById("listaContacto-click4").onmouseover = function () {
+    document.getElementById("listaContacto-click4").style.color = "#3AE4C7";
+    document.getElementById("listaContacto-click4").innerHTML = "Urquiza 32";
+}
+document.getElementById("listaContacto-click4").onmouseout = function () {
+    document.getElementById("listaContacto-click4").style.color = "#0a144d";
+    document.getElementById("listaContacto-click4").innerHTML = "Urquiza 32";
+}
+document.getElementById("listaContacto-click5").onmouseover = function () {
+    document.getElementById("listaContacto-click5").style.color = "#3AE4C7";
+    document.getElementById("listaContacto-click5").innerHTML = "Código postal 4610";
+}
+document.getElementById("listaContacto-click5").onmouseout = function () {
+    document.getElementById("listaContacto-click5").style.color = "#0a144d";
+    document.getElementById("listaContacto-click5").innerHTML = "Código Postal: 4610";
+}
